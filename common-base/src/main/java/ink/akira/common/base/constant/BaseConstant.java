79a7f4fdb0b9deaad5fe7f529ec517c23dc067d7
akira <moon_chaser@163.com>
2022-11-03 20:23:51 +0800

    deprecated

--- common-base/src/main/java/ink/akira/common/base/constant/BaseConstant.java
+++ common-base/src/main/java/ink/akira/common/base/constant/BaseConstant.java
@@ -5,7 +5,10 @@ package ink.akira.common.base.constant;
  *
  * @author chenhouzhang
  * @since 2022/8/29 23:19
+ * @deprecated
+ * @see ink.akira.common.base.enums.WhetherEnum
  */
+@Deprecated
 public class BaseConstant {
     public static final byte TRUE = 1;
     public static final byte FALSE = 0;
